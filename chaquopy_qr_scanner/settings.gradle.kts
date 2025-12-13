67dea9f251fd3c41de2d27ed462f5fa58be1ec26
QR Analyzer Developer <dev@qranalyzer.local>
2025-12-14 05:26:18 +0900

    Add Chaquopy Maven repository to settings.gradle.kts

--- chaquopy_qr_scanner/settings.gradle.kts
+++ chaquopy_qr_scanner/settings.gradle.kts
@@ -3,6 +3,9 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven {
+            url = uri("https://chaquo.com/maven")
+        }
     }
 }
 dependencyResolutionManagement {
