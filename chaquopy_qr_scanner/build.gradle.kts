dfba9f65c3c78908a3948320f1f57a1a49d54982
QR Analyzer Developer <dev@qranalyzer.local>
2025-12-14 05:32:47 +0900

    Fix Chaquopy plugin: use buildscript classpath instead of plugins block

--- chaquopy_qr_scanner/build.gradle.kts
+++ chaquopy_qr_scanner/build.gradle.kts
@@ -1,8 +1,20 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://chaquo.com/maven")
+        }
+    }
+    dependencies {
+        classpath("com.chaquo.python:gradle:15.0.1")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-    id("com.chaquo.python") version "15.0.1" apply false
 }
 
 tasks.register("clean", Delete::class) {
